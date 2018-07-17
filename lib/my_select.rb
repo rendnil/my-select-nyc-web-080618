1a7e3125edce94f37c5a8ab53bbe43205e861d8e
Andrew Lindner <andrew.lindner.15@gmail.com>
2018-07-17 13:11:49 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,22 @@
 def my_select(collection)
- # your code here!
+ 
+  i = 0
+  new_array = [ ]
+  
+  while i < collection.length
+  
+    x = yield collection[i]
+      if x == true
+        new_array << collection[i]
+      end
+    
+    i +=1
+  
+  end 
+  new_array
+ 
 end
+
+
+
+      
